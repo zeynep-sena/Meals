ffe7895c0e88035ee167c7549556acf6e009ff90
zeynep-sena <zsyurdadur@gmail.com>
2024-08-26 20:12:58 +0300

    commit

---
+++ lib/screens/categories.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
+import 'package:meals/screens/meals.dart';
+import 'package:meals/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+
+    final filteredMeals = dummyMeals.where((meal) => meal.categories.contains(category.id),).toList();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your category'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+        ),
+        children: [
+          for (final ctg in availableCategories)
+            CategoryGridItem(
+              category: ctg,
+              onSelectCategory: () {
+                _selectCategory(context, ctg);
+              },
+            ),
+        ],
+      ),
+    );
+  }
+}
