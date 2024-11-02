d53b31c258ba28951405a446c9c442d7154e4dd1
zeynep-sena <zsyurdadur@gmail.com>
2024-11-02 19:09:52 +0300

    added tab

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -8,8 +8,11 @@ class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
   void _selectCategory(BuildContext context, Category category) {
-
-    final filteredMeals = dummyMeals.where((meal) => meal.categories.contains(category.id),).toList();
+    final filteredMeals = dummyMeals
+        .where(
+          (meal) => meal.categories.contains(category.id),
+        )
+        .toList();
 
     Navigator.push(
       context,
@@ -24,28 +27,23 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pick your category'),
+    return GridView(
+      padding: const EdgeInsets.all(24),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20,
       ),
-      body: GridView(
-        padding: const EdgeInsets.all(24),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          mainAxisSpacing: 20,
-          crossAxisSpacing: 20,
-        ),
-        children: [
-          for (final ctg in availableCategories)
-            CategoryGridItem(
-              category: ctg,
-              onSelectCategory: () {
-                _selectCategory(context, ctg);
-              },
-            ),
-        ],
-      ),
+      children: [
+        for (final ctg in availableCategories)
+          CategoryGridItem(
+            category: ctg,
+            onSelectCategory: () {
+              _selectCategory(context, ctg);
+            },
+          ),
+      ],
     );
   }
 }
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:meals/models/meal.dart';
-import 'package:meals/screens/meal_detail.dart';
 import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
